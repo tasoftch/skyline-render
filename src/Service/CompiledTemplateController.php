a8cb155b781c1eaacb31f1ce4672cf8328bf6d57
Thomas Abplanalp <info@tasoft.ch>
2019-08-24 10:33:26 +0200

    FIX: Finding templates

--- src/Service/CompiledTemplateController.php
+++ src/Service/CompiledTemplateController.php
@@ -68,8 +68,8 @@ class CompiledTemplateController extends AbstractOrganizedTemplateController
     protected function yieldIDsInCatalog(string $name): Generator
     {
         if($names = $this->templateMeta["catalog"][$name] ?? NULL) {
-            foreach($names as $idx)
-                yield $this->_getTemplateID($idx);
+            foreach($names as $id => $idx)
+                yield $this->_getTemplateID($id);
         }
     }
 
